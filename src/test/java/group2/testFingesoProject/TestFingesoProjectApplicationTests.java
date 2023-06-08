553a37ad608c14ffe6d6e0e13b743082a9732a75
valenpy22 <valentina.campos.o@usach.cl>
2023-06-08 08:47:01 -0400

    Initial commit

---
+++ src/test/java/group2/testFingesoProject/TestFingesoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.testFingesoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFingesoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
