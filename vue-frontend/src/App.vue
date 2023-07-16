1ea5e97432021821885bd925bc463695573bed7b
Fabián Lizama <fabian.lizama@usach.cl>
2023-07-16 00:30:14 -0400

    feat: appBar implementation

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,21 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <appBar />
+  
 </template>
+
+<script>
+import appBar from './components/appBar.vue'
+export default {
+  components: {
+    appBar
+  }
+}
+</script>
 
 <style scoped>
 header {
---
+++ vue-frontend/src/components/appBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-layout>
+        <v-app-bar
+            flat
+            class="pa-md-3"
+            color="grey"
+        >
+            <v-app-bar-title>
+                <div class="mr-10 text-h2 text-white text-end font-weight-bold">
+                    MaiTickets
+                </div>
+            </v-app-bar-title>
+        </v-app-bar>
+    </v-layout>
+    
+</template>
+
+<script>
+export default {
+    name: 'appBar',
+    data: () => ({
+        })
+}
+
+</script>
