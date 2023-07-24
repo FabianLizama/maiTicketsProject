1195d48725890c089f9ca6fc283f5d88a481c18c
Fabián Lizama <you@example.com>
2023-07-23 22:36:34 -0400

    feat: db creation script added

---
+++ dbCreate.sql
@@ -0,0 +1,13 @@
+-- Database: maiticketsdb
+
+-- DROP DATABASE IF EXISTS maiticketsdb;
+
+CREATE DATABASE maiticketsdb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
